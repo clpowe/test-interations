5b71e31e65598defb78306c99cbed5c85300edaa
clpowe <clpowe@gmail.com>
2026-03-11 13:20:21 -0400

    stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite'
+import icon from 'astro-icon'
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+	vite: {
+		plugins: [tailwindcss()]
+	},
 
-  integrations: [react()]
-});
+	integrations: [
+		react(),
+		icon({
+			include: {
+				local: ['*'],
+				ph: ['*']
+			}
+		})
+	]
+})
